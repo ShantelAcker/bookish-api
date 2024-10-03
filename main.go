9d232e6c596c01d14d27ad5148913de5884028e9
Shantel Acker <shantelacker@gmail.com>
2024-10-02 23:12:37 -0500

    added endpoint to add books to a booklist

--- main.go
+++ main.go
@@ -42,7 +42,7 @@ func getBookLists(c *gin.Context) {
 	c.JSON(http.StatusOK, bookLists)
 }
 
-// middleware for getting any booklist ID
+// get any booklist by ID
 func bookListId(id string) (*bookList, error) {
 	for i, b := range bookLists {
 		if b.BooklistID == id {
@@ -68,7 +68,7 @@ func getBookListById(c *gin.Context) {
 func createBookList(c *gin.Context) {
 	var newBookList bookList
 
-	// bind JSON response to bookList
+	// bind JSON request body to bookList
 	if err := c.BindJSON(&newBookList); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
@@ -76,6 +76,27 @@ func createBookList(c *gin.Context) {
 
 	bookLists = append(bookLists, newBookList)
 	c.JSON(http.StatusCreated, newBookList)
+}
+
+// add a book to a booklist based on the booklist ID
+func createNewBook(c *gin.Context) {
+	// create the book variable that will be added to the booklist
+	var newBook book
+
+	// get the booklist to add the book to by id first
+	id := c.Param("id")
+	bookList, err := bookListId(id)
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"message": "booklist not found"})
+	}
+
+	if err := c.BindJSON(&newBook); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	bookList.BooklistBooks = append(bookList.BooklistBooks, newBook)
+	c.JSON(http.StatusCreated, bookList)
 }
 
 func main() {
@@ -88,11 +109,12 @@ func main() {
 	if err != nil {
 		fmt.Println("unable to unmarshal json")
 	}
-	fmt.Println(bookLists[0].BooklistBooks[0].BookAuthors[0].AuthorName)
+	// fmt.Println(bookLists[0].BooklistBooks[0].BookAuthors[0].AuthorName)
 
 	router := gin.Default()
 	router.GET("/booklists", getBookLists)
 	router.GET("/booklists/:id", getBookListById)
 	router.POST("/booklists", createBookList)
+	router.POST("/booklists/create/:id", createNewBook)
 	router.Run("localhost:8080")
 }
